5cd0f61b82936daea112cba79183ce6c570d4520
ajm4zs <alex_mulch@hotmail.com>
2020-01-16 01:10:01 -0500

    created simba page and added pics

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const async = require('async');
+
+const simbaPics = require('./../utils/simbaImages');
 
 router.get('/', function (req, res, next) {
 	res.render('home', {
@@ -32,9 +35,39 @@ router.get('/2048', function (req, res, next) {
 });
 
 router.get('/simba', function (req, res, next) {
-	res.render('simba', {
-		title: 'Alex Mulchandani'
+
+	const imgGroupings = {
+		family: [],
+		good: [],
+		bad: [],
+		adventure: [],
+		beach: [],
+		model: [],
+		needy: [],
+		sleepy: [],
+		play: []
+	};
+
+	async.forEachOf(simbaPics, function (pic, index, callback) {
+		imgGroupings[pic.class].push(pic);
+		callback();
+	}, function (err) {
+		if (err) return void next(err);
+		res.render('simba', {
+			title: 'Alex Mulchandani',
+			family: imgGroupings.family,
+			good: imgGroupings.good,
+			bad: imgGroupings.bad,
+			adventure: imgGroupings.adventure,
+			beach: imgGroupings.beach,
+			model: imgGroupings.model,
+			needy: imgGroupings.needy,
+			sleepy: imgGroupings.sleepy,
+			play: imgGroupings.play,
+			data: simbaPics
+		});
 	});
+
 });
 
 module.exports = router;
---
+++ utils/simbaImages.js
@@ -0,0 +1,831 @@
+const simbaPics = {
+	Simba_0: {
+		id: 0,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Big yawns!',
+		class: 'sleepy'
+	},
+	Simba_1: {
+		id: 1,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Arm rest or head rest?',
+		class: 'sleepy'
+	},
+	Simba_3: {
+		id: 3,
+		ext: 'jpg',
+		orientation: 'vertical',
+		caption: 'Licked my birthday dinner plate shiny clean!',
+		class: 'good'
+	},
+	Simba_7: {
+		id: 7,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Why sleep on my bed when I have a big couch',
+		class: 'sleepy'
+	},
+	Simba_8: {
+		id: 8,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Haircuts are so boring :(',
+		class: 'good'
+	},
+	Simba_9: {
+		id: 9,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Nothing like bathing in the Potomac',
+		class: 'adventure'
+	},
+	Simba_10: {
+		id: 10,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Play with me daddy!',
+		class: 'play'
+	},
+	Simba_12: {
+		id: 12,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Merry Christmas!',
+		class: 'model'
+	},
+	Simba_13: {
+		id: 13,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Yum, I love pig bones',
+		class: 'good'
+	},
+	Simba_14: {
+		id: 14,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Working on my tan',
+		class: 'sleepy'
+	},
+	Simba_15: {
+		id: 15,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Exploring Rock Creek with Momma',
+		class: 'family'
+	},
+	Simba_16: {
+		id: 16,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Stop working and play with me!',
+		class: 'needy'
+	},
+	Simba_17: {
+		id: 17,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Don\'t stare at the tv, stare at me',
+		class: 'needy'
+	},
+	Simba_18: {
+		id: 18,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Aren\'t I the goodest boy dad?',
+		class: 'good'
+	},
+	Simba_20: {
+		id: 20,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Backseat driving',
+		class: 'bad'
+	},
+	Simba_21: {
+		id: 21,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'My favorite spot',
+		class: 'needy'
+	},
+	Simba_22: {
+		id: 22,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Get out of bed dad!',
+		class: 'bad'
+	},
+	Simba_25: {
+		id: 25,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'zzZZZzzzz',
+		class: 'sleepy'
+	},
+	Simba_26: {
+		id: 26,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Do we have to leave the beach??',
+		class: 'model'
+	},
+	Simba_27: {
+		id: 27,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Maybe my puppy eyes will convince dad',
+		class: 'good'
+	},
+	Simba_29: {
+		id: 29,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Falling asleep sitting up',
+		class: 'sleepy'
+	},
+	Simba_30: {
+		id: 30,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'I love playing with my cousin!',
+		class: 'play'
+	},
+	Simba_32: {
+		id: 32,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'How\'s my new muzzle look?',
+		class: 'model'
+	},
+	Simba_35: {
+		id: 35,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Where are the deer to chase?',
+		class: 'adventure'
+	},
+	Simba_36: {
+		id: 36,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'GIVE ME ATTENTION!!',
+		class: 'needy'
+	},
+	Simba_38: {
+		id: 38,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'I love staring at dad',
+		class: 'needy'
+	},
+	Simba_39: {
+		id: 39,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'More staring at dad',
+		class: 'needy'
+	},
+	Simba_40: {
+		id: 40,
+		ext: 'jpg',
+		orientation: 'vertical',
+		caption: 'SEND IT!!!',
+		class: 'beach'
+	},
+	Simba_41: {
+		id: 41,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Getting ready to bike with Momma',
+		class: 'family'
+	},
+	Simba_42: {
+		id: 42,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'zzzzZZZZZzzzzzz',
+		class: 'sleepy'
+	},
+	Simba_43: {
+		id: 43,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Whatchu looking at, cat?',
+		class: 'good'
+	},
+	Simba_44: {
+		id: 44,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Posing for dad',
+		class: 'good'
+	},
+	Simba_47: {
+		id: 47,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'I love laying in inconvenient places',
+		class: 'sleepy'
+	},
+	Simba_49: {
+		id: 49,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Mountain views are boring',
+		class: 'adventure'
+	},
+	Simba_50: {
+		id: 50,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'My comfort zone',
+		class: 'needy'
+	},
+	Simba_51: {
+		id: 51,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Wake up dad!!',
+		class: 'needy'
+	},
+	Simba_52: {
+		id: 52,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Whatchu looking at?',
+		class: 'good'
+	},
+	Simba_53: {
+		id: 53,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Now that\'s tasty slobber',
+		class: 'bad'
+	},
+	Simba_55: {
+		id: 55,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'I guess we can share',
+		class: 'good'
+	},
+	Simba_56: {
+		id: 56,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Pet me!',
+		class: 'needy'
+	},
+	Simba_57: {
+		id: 57,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Nothing like drooling on this couch',
+		class: 'sleepy'
+	},
+	Simba_59: {
+		id: 59,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Are you gonna throw the ball or just take pics?',
+		class: 'play'
+	},
+	Simba_60: {
+		id: 60,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'It\'s my girlfriend, Miss Nala',
+		class: 'play'
+	},
+	Simba_61: {
+		id: 61,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Yes, I own this couch',
+		class: 'bad'
+	},
+	Simba_62: {
+		id: 62,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Come play with me and Miss Nala',
+		class: 'play'
+	},
+	Simba_64: {
+		id: 64,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Throw the ball already!',
+		class: 'model'
+	},
+	Simba_66: {
+		id: 66,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Gotta mark my territory',
+		class: 'bad'
+	},
+	Simba_68: {
+		id: 68,
+		ext: 'jpg',
+		orientation: 'vertical',
+		caption: 'Snuggles on dad\'s lap',
+		class: 'family'
+	},
+	Simba_69: {
+		id: 69,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'I think I\'ll just lay in the middle of the crosswalk',
+		class: 'bad'
+	},
+	Simba_70: {
+		id: 70,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'I hate bath time',
+		class: 'bad'
+	},
+	Simba_71: {
+		id: 71,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Self-imposed timeout',
+		class: 'bad'
+	},
+	Simba_72: {
+		id: 74,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Beach boy for life',
+		class: 'beach'
+	},
+	Simba_73: {
+		id: 73,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Steak and sweet potatoes for the birtday boy!',
+		class: 'good'
+	},
+	Simba_77: {
+		id: 77,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'I use my own bed sometimes...',
+		class: 'sleepy'
+	},
+	Simba_78: {
+		id: 78,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'I missed you so much dad',
+		class: 'needy'
+	},
+	Simba_79: {
+		id: 79,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Like my rain jacket?',
+		class: 'model'
+	},
+	Simba_81: {
+		id: 81,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'What a comfy couch',
+		class: 'sleepy'
+	},
+	Simba_83: {
+		id: 83,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Are we there yet???',
+		class: 'good'
+	},
+	Simba_87: {
+		id: 87,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Aunty is in my spot so I guess I have to sit on her',
+		class: 'bad'
+	},
+	Simba_90: {
+		id: 90,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'That\'s the spot',
+		class: 'needy'
+	},
+	Simba_91: {
+		id: 91,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Peak a boo',
+		class: 'needy'
+	},
+	Simba_92: {
+		id: 92,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Am I a good boy or am I a good boy',
+		class: 'play'
+	},
+	Simba_93: {
+		id: 93,
+		ext: 'jpg',
+		orientation: 'vertical',
+		caption: 'Post hike snuggles on dad',
+		class: 'sleepy'
+	},
+	Simba_94: {
+		id: 94,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Dreaming about this darn squirrels',
+		class: 'sleepy'
+	},
+	Simba_97: {
+		id: 97,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'This seems like a nice spot to lay',
+		class: 'family'
+	},
+	Simba_98: {
+		id: 98,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'I\'m quite a hairy guy',
+		class: 'good'
+	},
+	Simba_100: {
+		id: 100,
+		ext: 'jpeg',
+		orientation: 'vertical',
+		caption: 'Attention please!',
+		class: 'needy'
+	},
+	Simba_2: {
+		id: 2,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Can we play yet',
+		class: 'needy'
+	},
+	Simba_4: {
+		id: 4,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Cotton candy sunsets at the vineyards',
+		class: 'model'
+	},
+	Simba_5: {
+		id: 5,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Do not disturb mode activated',
+		class: 'sleepy'
+	},
+	Simba_6: {
+		id: 6,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Pondering life with Momma',
+		class: 'family'
+	},
+	Simba_11: {
+		id: 11,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: '#views',
+		class: 'adventure'
+	},
+	Simba_19: {
+		id: 19,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Can I get back to swimming yet?',
+		class: 'model'
+	},
+	Simba_23: {
+		id: 23,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'I love hikes to rivers!',
+		class: 'adventure'
+	},
+	Simba_24: {
+		id: 24,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'zzzzZZZZZzzzz',
+		class: 'sleepy'
+	},
+	Simba_33: {
+		id: 33,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Posing in the Gulf of Mexico',
+		class: 'model'
+	},
+	Simba_34: {
+		id: 34,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Baewatch with my uncle',
+		class: 'beach'
+	},
+	Simba_37: {
+		id: 37,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'I dig the best holes',
+		class: 'bad'
+	},
+	Simba_45: {
+		id: 82,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Beaches are for swimming and digging massive holes',
+		class: 'beach'
+	},
+	Simba_46: {
+		id: 118,
+		ext: 'jpg',
+		orientation: 'horizontal',
+		caption: 'Why kayak when you can swim',
+		class: 'adventure'
+	},
+	Simba_48: {
+		id: 48,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'How relaxing',
+		class: 'sleepy'
+	},
+	Simba_58: {
+		id: 58,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'UVA won the championship!',
+		class: 'model'
+	},
+	Simba_63: {
+		id: 63,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Yea I know I\'m cute',
+		class: 'model'
+	},
+	Simba_65: {
+		id: 65,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Snuggles with my sticks and bones',
+		class: 'sleepy'
+	},
+	Simba_74: {
+		id: 72,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Let\'s lay here all night!',
+		class: 'beach'
+	},
+	Simba_75: {
+		id: 75,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'The water is way more interesting than the stinking cherry blossoms',
+		class: 'adventure'
+	},
+	Simba_76: {
+		id: 76,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'My old stomping grounds!',
+		class: 'model'
+	},
+	Simba_82: {
+		id: 45,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Nothing like sandy summer days',
+		class: 'beach'
+	},
+	Simba_84: {
+		id: 84,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Who knew the couch could get more comfy with pillows',
+		class: 'sleepy'
+	},
+	Simba_85: {
+		id: 85,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Posing with Momma',
+		class: 'family'
+	},
+	Simba_86: {
+		id: 86,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Which stick do I grab first??',
+		class: 'adventure'
+	},
+	Simba_88: {
+		id: 88,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Baewatch with my unlce pt. 2',
+		class: 'beach'
+	},
+	Simba_89: {
+		id: 89,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Nothing like an afternoon nap',
+		class: 'sleepy'
+	},
+	Simba_95: {
+		id: 95,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Putting my head in forbidden places',
+		class: 'bad'
+	},
+	Simba_96: {
+		id: 96,
+		ext: 'jpeg',
+		orientation: 'horizontal',
+		caption: 'Lazy mornings with my uncle',
+		class: 'family'
+	},
+	Simba_101: {
+		id: 101,
+		ext: 'jpg',
+		orientation: 'other',
+		caption: 'If only I could see...',
+		class: 'good'
+	},
+	Simba_102: {
+		id: 102,
+		ext: 'jpg',
+		orientation: 'other',
+		caption: 'Manicure please',
+		class: 'needy'
+	},
+	Simba_103: {
+		id: 103,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Paws for days',
+		class: 'model'
+	},
+	Simba_104: {
+		id: 104,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Come play little friend',
+		class: 'play'
+	},
+	Simba_105: {
+		id: 105,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Granddad says I look more like Nala than Simba',
+		class: 'model'
+	},
+	Simba_106: {
+		id: 106,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'King of the forest',
+		class: 'adventure'
+	},
+	Simba_107: {
+		id: 107,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'If I don\'t get attention, you can\'t workout',
+		class: 'bad'
+	},
+	Simba_108: {
+		id: 108,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Throw another snowball please!',
+		class: 'play'
+	},
+	Simba_109: {
+		id: 109,
+		ext: 'jpg',
+		orientation: 'other',
+		caption: 'Duelin\' with friends',
+		class: 'play'
+	},
+	Simba_110: {
+		id: 110,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Got caught up admiring the cherry blossoms',
+		class: 'model'
+	},
+	Simba_111: {
+		id: 111,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Strolls with grandma',
+		class: 'family'
+	},
+	Simba_112: {
+		id: 112,
+		ext: 'jpg',
+		orientation: 'other',
+		caption: 'Maybe this will fix your knee dad',
+		class: 'good'
+	},
+	Simba_114: {
+		id: 114,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Car rides are better laying on dad',
+		class: 'sleepy'
+	},
+	Simba_115: {
+		id: 115,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Best time of the year!',
+		class: 'play'
+	},
+	Simba_116: {
+		id: 116,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Please give a treat to the bestest boy',
+		class: 'model'
+	},
+	Simba_117: {
+		id: 117,
+		ext: 'jpg',
+		orientation: 'other',
+		caption: 'Mud is fun',
+		class: 'bad'
+	},
+	Simba_118: {
+		id: 46,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Gotta protect dad from the deer',
+		class: 'adventure'
+	},
+	Simba_119: {
+		id: 119,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'You\'re sitting in my spot, dad',
+		class: 'needy'
+	},
+	Simba_120: {
+		id: 120,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Tani Auntie!',
+		class: 'family'
+	},
+	Simba_121: {
+		id: 121,
+		ext: 'jpeg',
+		orientation: 'other',
+		caption: 'Why do I resemble a mop',
+		class: 'model'
+	},
+	Simba_122: {
+		id: 122,
+		ext: 'jpg',
+		orientation: 'other',
+		caption: 'Dad, you\'re embarrassing me!',
+		class: 'family'
+	},
+	// Simba_123: {
+	// 	id: 123,
+	// 	ext: 'jpg',
+	// 	orientation: 'other',
+	// 	caption: 'Viraat came to visit from India!',
+	// 	class: 'family'
+	// },
+	Simba_124: {
+		id: 124,
+		ext: 'jpg',
+		orientation: 'other',
+		caption: 'Snuggle time with dad',
+		class: 'good'
+	}
+
+};
+
+module.exports = simbaPics;
